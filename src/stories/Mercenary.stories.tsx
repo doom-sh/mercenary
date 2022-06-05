6bf06ca326a4fd20a1865078dc986679cfd71e24
Sarim Abbas <sarim.stays@gmail.com>
2022-06-04 18:44:34 -0700

    cleanup

--- src/stories/Mercenary.stories.tsx
+++ src/stories/Mercenary.stories.tsx
@@ -33,4 +33,5 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   markdown: sampleMarkdown,
+  className: "",
 };
