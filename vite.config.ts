8596779f9dadc1106096793c1bf9497ed1039e0b
Sarim Abbas <sarim.stays@gmail.com>
2022-06-04 21:12:37 -0700

    emit types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   // when building, output as library
   build: {
     lib: {
